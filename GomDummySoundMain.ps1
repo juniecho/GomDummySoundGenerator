1e6010dfe105f69cc88a0f7478dc08db08bba644
Gomdolius <juniecho@gomdoli.us>
2020-05-05 22:17:35 +0900

    Add files via upload

---
+++ GomDummySoundMain.ps1
@@ -0,0 +1,13 @@
+﻿$PowerEventInfo=EventLog -LogName System -Source Microsoft-Windows-Power-Troubleshooter -Newest 1 | Out-String
+$PowerEventInfoCurrent=$PowerEventInfo
+$Player=New-Object Media.SoundPlayer \"silence.wav\"
+
+while ($true) {
+	$Player.PlayLooping()
+	while ($PowerEventInfo -like $PowerEventInfoCurrent) {
+		$PowerEventInfoCurrent=EventLog -LogName System -Source Microsoft-Windows-Power-Troubleshooter -Newest 1 | Out-String
+		Start-Sleep -s 15
+	}
+	$PowerEventInfo=$PowerEventInfoCurrent
+	$Player.Stop()
+}
